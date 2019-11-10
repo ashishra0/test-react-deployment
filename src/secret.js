d98367fc1bc4c526c4669ab4875339773c338b4a
Ashish Rao <ashishrao2598@gmail.com>
2019-11-10 18:59:16 +0530

    initial commit

---
+++ src/secret.js
@@ -0,0 +1,3 @@
+export const GRAPHQL_URL = 'https://react-auth0-hasura.herokuapp.com/v1/graphql';
+export const authClientId = process.env.REACT_APP_AUTH_CLIENT_ID
+export const authDomain = "todo-test.auth0.com";
